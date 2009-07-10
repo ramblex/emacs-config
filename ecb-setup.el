c08b72b2df3b015a9222b1e64a3830c05f03415e
Alex Duller <alexduller@gmail.com>
2009-07-10 21:45:18 +0100

    Initial commit

---
+++ ecb-setup.el
@@ -0,0 +1,20 @@
+(load-file (concat package-user-dir "/cedet/common/cedet.el"))
+
+(add-to-list 'load-path (concat package-user-dir "/ecb"))
+(require 'ecb)
+
+(defun my-ecb-activate-hook ()
+  (modify-all-frames-parameters '((width . 120)))
+  (ecb-redraw-layout))
+
+(add-hook 'ecb-activate-hook 'my-ecb-activate-hook)
+
+(defun my-ecb-deactivate-hook ()
+  (modify-all-frames-parameters '((width . 80))))
+(add-hook 'ecb-deactivate-hook 'my-ecb-deactivate-hook)
+
+(setq ecb-primary-secondary-mouse-buttons (quote mouse-1--mouse-2)
+      ecb-tip-of-the-day nil
+      ecb-options-version "2.40")
+
+(provide 'ecb-setup)
