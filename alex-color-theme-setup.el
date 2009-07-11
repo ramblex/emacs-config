86138bb1d583680d74ca4f6bf3efc36fcd46f196
Alex Duller <alexduller@gmail.com>
2009-07-11 17:01:27 +0100

    Made changes to work with alex- prefix

--- alex-color-theme-setup.el
+++
@@ -1,7 +0,0 @@
-(add-to-list 'load-path (concat package-user-dir "/color-theme"))
-(require 'color-theme)
-(require 'my-color-themes)
-(color-theme-initialize)
-(color-theme-railscasts)
-
-(provide 'color-theme-setup)
