b4413c1cdaed36953c1448334dc19bf5966e2e7e
Alex Duller <alexduller@gmail.com>
2009-07-12 22:01:21 +0100

    Changed colours

--- alex-color-theme.el
+++ alex-color-theme.el
@@ -7,9 +7,8 @@
   (color-theme-install
    '(color-theme-railscasts
      ((background-color . "#2b2b2b")
-      (background-mode . dark)
       (foreground-color . "#ffffff")
-      (cursor-color . "#5b5b5b"))
+      (cursor-color . "#6b6b6b"))
      (hl-line ((t (:background "#262626"))))
      (region ((t (:background "#5a647e"))))
      (highlight ((t (:background "#4b4b4b"))))
@@ -21,27 +20,24 @@
      (minibuffer-prompt ((t (:bold t :foreground "#708090"))))
      (font-lock-builtin-face ((t (:foreground "#8ac6f2"))))
      (font-lock-comment-face ((t (:italic t :foreground "#bc9458"))))
-     (font-lock-constant-face ((t (:foreground "#d0d0ff"))))
+     (font-lock-constant-face ((t (:foreground "#6e9cbe"))))
      (font-lock-doc-face ((t (:foreground "#a5c261"))))
      (font-lock-function-name-face ((t (:foreground "#ffc66d"))))
      (font-lock-keyword-face ((t (:foreground "#cc7833"))))
      (font-lock-preprocessor-face ((t (:foreground "d0d0ff"))))
      (font-lock-reference-face ((t (:bold nil :foreground "#808bed"))))
      (font-lock-string-face ((t (:foreground "#a5c261"))))
-     (font-lock-type-face ((t (:foreground "#d0d0ff"))))
-     (font-lock-variable-name-face ((t (:foreground "#6e9cbe"))))
+     (font-lock-type-face ((t (:foreground "#ffffff"))))
+     (font-lock-variable-name-face ((t (:foreground "#d0d0ff"))))
      (font-lock-warning-face
       ((t (:foreground "#ffffff" :background "#da4939"))))
      (rails-font-lock-function-face ((t (:foreground "#da4939"))))
-     (erb-delim-face ((t (:bold nil :italic nil :foreground "#ffffff"
-                                :background "#242424"))))
-     (erb-out-delim-face ((t (:bold nil :italic nil :foreground "#ffffff"
-                                    :background "#242424"))))
+     (erb-delim-face ((t (:normal t :foreground "#ffffff" :background "#242424"))))
+     (erb-exec-delim-face ((t (:normal t :foreground "#ffffff" :background "#242424"))))
+     (erb-out-delim-face ((t (:normal t :foreground "#ffffff" :background "#242424"))))
      (erb-face ((t (:background "#242424"))))
      (ecb-default-highlight-face ((t (:background "#5a647e"
                                                   :foreground "#f6f3e8"))))
-     (ecb-default-general-face ((t ())))
-     (ecb-directories-general-face ((t ())))
      (yas/field-highlight-face ((t (:background "grey13"))))
      )))
 
