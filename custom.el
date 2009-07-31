845b6199c34b6db387e96b30d067a61fe1c86736
Alex Duller <alexduller@gmail.com>
2009-07-31 20:53:41 +0100

    Tracking custom.el

---
+++ custom.el
@@ -0,0 +1,19 @@
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(c-basic-offset 2)
+ '(c-default-style (quote ((java-mode . "java") (other . "k&r"))))
+ '(ecb-layout-name "right1")
+ '(ecb-layout-window-sizes (quote (("right1" (0.3333333333333333 . 0.275) (0.3333333333333333 . 0.35) (0.3333333333333333 . 0.35)))))
+ '(ecb-options-version "2.40")
+ '(ecb-source-path (quote ("~")))
+ '(ns-alternate-modifier (quote none))
+ '(ns-command-modifier (quote meta)))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
