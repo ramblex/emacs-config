120aee1be44b9ecc8b821f35bcdcb3d74b3d5759
Alex Duller <alexduller@gmail.com>
2009-08-20 18:59:48 +0100

    Got rid of extra / in package-user-dir

--- init.el
+++ init.el
@@ -12,10 +12,10 @@
       (or (buffer-file-name) load-file-name)))
 
 (add-to-list 'load-path dotfiles-dir)
-(add-to-list 'load-path (concat dotfiles-dir "/vendor"))
+(add-to-list 'load-path (concat dotfiles-dir "vendor"))
 
 (setq custom-file (concat dotfiles-dir "custom.el"))
-(setq package-user-dir (concat dotfiles-dir "/vendor"))
+(setq package-user-dir (concat dotfiles-dir "vendor"))
 
 ;; These should be loaded on startup rather than autoloaded on demand
 ;; since they are likely to be used in every session
