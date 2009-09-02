cadb278af1fea62751a21ce9468d2b9ff6744cb8
Alex Duller <alexduller@gmail.com>
2009-09-02 18:04:06 +0100

    Added hook to magit to disable yas.

--- alex-magit.el
+++ alex-magit.el
@@ -1,4 +1,9 @@
 (add-to-list 'load-path (concat package-user-dir "/magit"))
 (require 'magit)
 
+(defun disable-yas ()
+  (setq yas/dont-activate t))
+
+(add-hook 'magit-mode-hook 'disable-yas)
+
 (provide 'alex-magit)
