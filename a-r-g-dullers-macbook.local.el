15cd2b14ffb291c8c1494c26dbce1d0c09a8c0de
Alex Duller <alexduller@gmail.com>
2009-09-09 10:44:31 +0100

    Added two extra directories to the emacs PATH on macbook

--- a-r-g-dullers-macbook.local.el
+++ a-r-g-dullers-macbook.local.el
@@ -1,8 +1,10 @@
 (setq mac-pass-command-to-system nil)
 (setq ns-alternate-modifier (quote none))
 (setq ns-command-modifier (quote meta))
-(setenv "PATH" (concat (getenv "PATH") ":/sw/bin"))
-(setq exec-path (append exec-path '("/sw/bin")))
+(setenv "PATH" (concat (getenv "PATH") ":/sw/bin:/usr/local/bin:/usr/texbin"))
+(setq exec-path (append exec-path '("/sw/bin" 
+                                    "/usr/local/bin" 
+                                    "/usr/texbin")))
 
 (defun bja-growl-notification (title message &optional sticky)
   "Send a Growl notification"
