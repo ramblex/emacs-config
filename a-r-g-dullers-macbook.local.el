41bf249b8dd321e62c90f32c2715dfa0f0367577
Alex Duller <alexduller@gmail.com>
2009-09-16 21:54:40 +0100

    Added d-mode

--- a-r-g-dullers-macbook.local.el
+++ a-r-g-dullers-macbook.local.el
@@ -1,10 +1,13 @@
 (setq mac-pass-command-to-system nil)
 (setq ns-alternate-modifier (quote none))
 (setq ns-command-modifier (quote meta))
-(setenv "PATH" (concat (getenv "PATH") ":/sw/bin:/usr/local/bin:/usr/texbin"))
+(setenv "PATH" (concat (getenv "PATH") 
+                       ":/sw/bin:/usr/local/bin:/usr/texbin"
+                       ":/Users/alexduller/dmd/osx/bin"))
 (setq exec-path (append exec-path '("/sw/bin" 
                                     "/usr/local/bin" 
-                                    "/usr/texbin")))
+                                    "/usr/texbin"
+                                    "/Users/alexduller/dmd/osx/bin")))
 
 (defun bja-growl-notification (title message &optional sticky)
   "Send a Growl notification"
