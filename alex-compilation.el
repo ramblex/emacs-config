2d91912aa52c5be2984dd4d3e03445e6dd4b6c79
Alex Duller <alexduller@gmail.com>
2009-09-26 00:59:16 +0100

    Added Passed word to compilation mode highlighting

--- alex-compilation.el
+++ alex-compilation.el
@@ -1,7 +1,7 @@
 (font-lock-add-keywords
  'compilation-mode
  '(("\\(\\[ FAIL \\]\\)" 1 compilation-error-face prepend)
-   ("\\(Failed\\)" 1 compilation-error-face prepend)))
-compilation-mode-font-lock-keywords
+   ("\\(Failed\\)" 1 compilation-error-face prepend)
+   ("\\(Passed\\)" 1 compilation-info)))
 
 (provide 'alex-compilation)
