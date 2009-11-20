f3ba2a7f62734e755afa063cdbade3d39a6675a1
Alex Duller <alexduller@gmail.com>
2009-11-20 21:57:18 +0000

    Updated yasnippet to 0.6.1c

--- my-tab-complete.el
+++ my-tab-complete.el
@@ -1,17 +1,20 @@
+(add-to-list 'load-path (concat package-user-dir "/yasnippet"))
 (setq snippets-dir (concat dotfiles-dir "snippets"))
 
 (require 'yasnippet)
 (yas/initialize)
 (yas/load-directory snippets-dir)
 
-(setq yas/fallback-behavior 'run-hooks)
 (defun run-hippie-expand ()
+  (interactive)
   (ignore-errors (hippie-expand nil)))
-(add-hook 'yas/snippet-not-found-hook 'run-hippie-expand)
+
+(global-set-key [tab] 'run-hippie-expand)
 
 ;; Hippie expand
 (setq hippie-expand-try-functions-list
-      '(try-expand-dabbrev
+      '(yas/hippie-try-expand
+        try-expand-dabbrev
         try-expand-dabbrev-all-buffers
  	try-expand-dabbrev-from-kill
  	try-complete-file-name-partially
