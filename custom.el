82eedaf3081997127494e52f5e13627cfdb0e05c
Alex Duller <alexduller@gmail.com>
2009-11-22 21:29:48 +0000

    Removed scroll bars

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
+ '(blink-cursor-mode t)
  '(c-basic-offset (quote set-from-style))
  '(c-default-style (quote ((java-mode . "java"))))
  '(compilation-scroll-output t)
@@ -12,6 +13,7 @@
  '(ecb-source-path (quote ("~")))
  '(ns-alternate-modifier (quote none))
  '(ns-command-modifier (quote meta))
+ '(scroll-bar-mode nil)
  '(tm/backspace-delete-column nil))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
