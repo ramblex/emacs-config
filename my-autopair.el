2ef041e17617cb4928fb4134a77d90e0c398b394
Alex Duller <alexduller@gmail.com>
2009-11-26 16:04:26 +0000

    Updated description for my-autopair function. Renamed the opening char.

--- my-autopair.el
+++ my-autopair.el
@@ -3,17 +3,19 @@
 (autopair-global-mode)
 
 (defun my-autopair-action (action pair pos-before)
-  "Trying to emulate the autowrap functionality from skeleton-insert-maybe"
+  "Trying to emulate the autowrap functionality from
+skeleton-insert-maybe. When the region is active, wrap the pair
+around the region, otherwise use the default action"
   (if (or (eq last-command 'mouse-drag-region)
           (and transient-mark-mode mark-active))
     (progn
       (if (< (region-beginning) pos-before)
-          ;; Move opening pair to region-beginning
-          (let ((opening (char-before)))
+          ;; Make sure that pair opening is at region-beginning
+          (let ((open-char (char-before)))
             (delete-backward-char 1)
             (save-excursion
               (goto-char (region-beginning))
-              (insert opening))))
+              (insert open-char))))
       ;; Insert the closing pair at the end of the region
       (save-excursion
         (goto-char (region-end))
