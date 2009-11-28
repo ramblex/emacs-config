d8a244441a02ee30c3c6cae6901c53e32a013154
Alex Duller <alexduller@gmail.com>
2009-11-28 12:07:59 +0000

    Removed my-textmate

--- my-textmate.el
+++
@@ -1,9 +0,0 @@
-;; Use textmate style auto-insert of parens
-(add-to-list 'load-path (concat package-user-dir "/textmate"))
-
-(require 'textmate)
-(tm/initialize)
-(setq tm/use-newline-and-indent t)
-(setq tm/use-goto-line nil)
-
-(provide 'my-textmate)
