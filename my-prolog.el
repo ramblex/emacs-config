cd447640f1f1891e5bf8aef438ce157a5536c8fe
Alex Duller <alexduller@gmail.com>
2010-04-05 10:18:10 +0100

    Turn off prolog auto-mode thingy

--- my-prolog.el
+++ my-prolog.el
@@ -1,6 +1,6 @@
 (require 'prolog)
 (setq prolog-system 'swi)
-(add-to-list 'auto-mode-alist '("\\.pl$" . prolog-mode))
+;(add-to-list 'auto-mode-alist '("\\.pl$" . prolog-mode))
 (setenv "EPROLOG" "/opt/local/bin/swipl")
 (setq prolog-program-name "swipl")
 
